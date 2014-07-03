924b64d2f7dd9db0482a824dd68fd3573b6f8cd3
Aaron Morais <air@fb.com>
2014-07-03 00:18:19 -0700

    Update the client side url

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,6 @@
 window.onload = function() {
-	var socket = io.connect('http://localhost:3000');
+	var url = "http://aaronmorais.com:3000"
+	var socket = io.connect(url);
 	socket.emit('subscribe', {"location" : "toronto"});
 	socket.on('photos', function (data) {
 		console.log(data);
