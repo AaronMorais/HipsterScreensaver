23f2f78ed00c7474a4b1e3990715832e23e547cd
Aaron Morais <air@fb.com>
2014-07-07 23:14:40 -0700

    Distance proper

--- app.js
+++ app.js
@@ -116,16 +116,34 @@ function broadcastDataForLocation (location, data) {
 }
 
 function addLocation(lat, lng) {
-  api.add_geography_subscription(lat, lng, 5000, 
+  api.add_geography_subscription(lat, lng, 5000,
       url + '/geography', function(err, result, limit) {});
 }
 
 function getCity(lat, lng) {
+  var smallestCity;
+  var smallestDistance = -1;
   for (var city in supported_locations) {
-    if (supported_locations[city]["latitude"] - lat < 1 
-        && supported_locations[city]["longitude"] - lng < 1) {
-      console.log(city);
-      return city; 
-    }     
+    var cityDistance = distance(lat, lng, supported_locations[city]["latitude"], supported_locations[city]["longitude"]);
+    if (cityDistance < smallestDistance) {
+      cityDistance = smallestDistance;
+      smallestCity = supported_locations[city];
+    }
   }
+  return smallestCity;
+}
+
+function distance(lat1, lon1, lat2, lon2) {
+  var R = 6371; // km
+  var φ1 = lat1.toRadians();
+  var φ2 = lat2.toRadians();
+  var Δφ = (lat2-lat1).toRadians();
+  var Δλ = (lon2-lon1).toRadians();
+
+  var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+          Math.cos(φ1) * Math.cos(φ2) *
+          Math.sin(Δλ/2) * Math.sin(Δλ/2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+  var d = R * c;
 }
